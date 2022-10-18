87654daf3113de8848684d7958427ab56d01e0f9
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-10-18 09:07:54 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/components/Page2.js
    	modified:   src/data.json

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -5,11 +5,12 @@ export const Page2 = () => {
   return (
     <div>
       Page2
-      <Grid data={product} />
-      <GridColumn field="id" title="Id" />
-      <GridColumn field="title" title="Name" />
-      <GridColumn field="price" title="Price" />
-      <GridColumn field="brand" title="Brand" />
+      <Grid data={product}>
+        <GridColumn field="id" title="Id" />
+        <GridColumn field="title" title="Name" />
+        <GridColumn field="price" title="Price" />
+        <GridColumn field="brand" title="Brand" />
+      </Grid>
     </div>
   );
 };
