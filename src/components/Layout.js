667438575277f0921c07aa5f479b0e27b7949eb8
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-10-26 18:21:19 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   src/components/Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,22 @@
+import {
+  AppBar,
+  AppBarSection,
+  AppBarSpacer,
+} from "@progress/kendo-react-layout";
 import React from "react";
 import { Link } from "react-router-dom";
 export const Layout = () => {
   return (
     <div>
-      <Link to="Page1">Page1</Link>
-      <Link to="Page2">Page2</Link>
-      <Link to="BottomNav">BottomNav</Link>
+      <AppBar positionMode="sticky">
+        <AppBarSection themeColor="light">
+          <Link to="Page1">Page1</Link>
+          <AppBarSpacer style={{ width: 50 }} />
+          <Link to="Page2">Page2</Link>
+          <AppBarSpacer style={{ width: 8 }} />
+          <Link to="BottomNav">BottomNav</Link>
+        </AppBarSection>
+      </AppBar>
     </div>
   );
 };
